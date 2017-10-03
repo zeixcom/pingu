1b8e807481b0f7e070e6a9b0948d74137c338399
Marcel Bührig <marcel.buehrig@zeix.com>
2017-10-03 11:52:39 +0200

    Fixed pull request reviews added todos for future global path handling

--- config/gulp/twig.js
+++ config/gulp/twig.js
@@ -7,10 +7,10 @@ gulp.task('twig', function() {
   var rename = require('gulp-rename');
   var yaml = require('js-yaml');
   var data = require('gulp-data');
-  var gutil = require('gulp-util');
   var path = require('path');
   var fs = require('fs');
 
+  // @TODO: Replace to global path handling ('src/pages/');
   return gulp.src(['./src/pages/**/*.twig'])
     .pipe(data(function(file) {
       var filename = path.basename(file.path, path.extname(file.path));
@@ -22,6 +22,7 @@ gulp.task('twig', function() {
       {dirname: ''}
     ))
     .pipe(
+      // @TODO: Replace to global Path Handling
       gulp.dest('./.tmp/')
     );
 });
