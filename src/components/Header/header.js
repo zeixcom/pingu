3770ec8035c1a8a6cdd505e8238b64c1b7b3d16a
Samuel Tschümperlin <samuel.tschuemperlin@zeix.com>
2017-10-05 11:28:11 +0200

    PNG-9 add main.js, add components js, scss

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
 export default class Header {}
+=======
+export default class Header {
+  constructor() {
+    console.log('header.js loaded');
+  }
+}
+>>>>>>> PNG-9 add main.js, add components js, scss
