7d449eca974e99f8178ac0fcf53ee2d358eda099
Marcel Bührig <marcel.buehrig@zeix.com>
2017-10-05 18:20:16 +0200

    Rework in progress of gulp add generator

--- config/scaffold/add.js
+++ config/scaffold/add.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 var prompt = require('gulp-prompt');
 var gutil = require('gulp-util');
+var _ = require('lodash');
+var tap = require('gulp-tap');
+var path = require('path');
+var yaml = require('js-yaml');
+
 var scaffoldUtils = require('./scaffold.utils.js');
 
-gulp.task('addGenerator', function() {
+var nodeName = {};
+var nodeType = '';
+
+gulp.task('addGeneratorPrompts', function() {
   return gulp.src('./config/scaffold/add.js')
     .pipe(prompt.prompt([{
       type: 'list',
@@ -15,16 +23,28 @@ gulp.task('addGenerator', function() {
       name: 'nodeName',
       message: 'Name?'
     }], function(res) {
-      var nodeName = res.nodeName.toLowerCase();
+      nodeName = scaffoldUtils.normalizeNodeName(res.nodeName);
+      nodeType = res.nodeType;
+    }))
+});
 
-      switch(res.nodeType) {
-        case 'Page':
-          scaffoldUtils.generateNewPage(nodeName);
+gulp.task('addGenerator', ['addGeneratorPrompts'], function() {
+  var folder = nodeType.toLowerCase();
+
+  return gulp.src(`./config/scaffold/${folder}/*.{js,twig,yml,scss}`)
+    .pipe(tap(function(file, t) {
+      var extension = path.extname(file.path);
+
+      switch(extension) {
+        case '.yml':
+
+
+
           break;
-        case 'Layout':
-          scaffoldUtils.generateNewLayout(nodeName);
+        default:
           break;
       }
     }))
-});
+    .pipe(gulp.dest(`./src/${folder}s/${nodeName.directory}/`))
+})
 
