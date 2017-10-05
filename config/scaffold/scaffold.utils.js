a8c7ba6c26529e367ab93945fff058aa730e65ce
Marcel Bührig <marcel.buehrig@zeix.com>
2017-10-05 14:18:40 +0200

    Added generators for layouts and pages

---
+++ config/scaffold/scaffold.utils.js
@@ -0,0 +1,40 @@
+var fs = require('fs-extra');
+var path = require('path');
+var yaml = require('js-yaml');
+
+exports.ADDABLE_NODES = ['Component', 'Layout', 'Page']
+
+/**
+ * Turns a nodeName into a directoryName
+ */
+exports.getDirectoryName = function(nodeName) {
+  const nodeNameLowerCased = nodeName.toLowerCase();
+
+  return nodeNameLowerCased[0].toUpperCase() + nodeNameLowerCased.substring(1);
+}
+
+/**
+ * Generating a new page from scaffold templates
+ */
+exports.generateNewPage = function(nodeName) {
+  const directory = `./src/pages/${this.getDirectoryName(nodeName)}/`;
+
+  const yamlTpl = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, './page/page.yml'), 'utf8'));
+
+  yamlTpl.config.title = nodeName;
+
+  fs.openSync(`${directory}${nodeName}.yml`, 'w');
+
+  fs.writeFile(`${directory}${nodeName}.yml`, yaml.safeDump(yamlTpl));
+  fs.copySync(path.resolve(__dirname, './page/page.twig'), `${directory}${nodeName}.twig`);
+}
+
+/**
+ * Generating a new layout, from scaffold templates
+ */
+exports.generateNewLayout = function(nodeName) {
+  const directory = `./src/layouts/${this.getDirectoryName(nodeName)}/`;
+
+  fs.copySync(path.resolve(__dirname, './layout/layout.twig'), `${directory}${nodeName}.twig`);
+  fs.copySync(path.resolve(__dirname, './layout/layout.yml'), `${directory}${nodeName}.yml`);
+}
