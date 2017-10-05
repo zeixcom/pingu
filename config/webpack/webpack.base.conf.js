8f5f3c3842f0da0d515c2299c00a2f3398155bd6
Samuel Tschümperlin <samuel.tschuemperlin@zeix.com>
2017-10-05 11:27:30 +0200

    PNG-9 add webpack

---
+++ config/webpack/webpack.base.conf.js
@@ -0,0 +1,30 @@
+var path = require('path');
+// var utils = require('./utils.webpack.js');
+
+function resolve (dir) {
+  return path.join(__dirname, '../../', dir)
+}
+
+module.exports = {
+  entry: ['./src/assets/main.js'],
+  output: {
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+	test: /\.js$/,
+	loader: 'eslint-loader',
+	enforce: 'pre',
+	options: {
+	  formatter: require('eslint-formatter-pretty')
+	}
+      },
+      {
+	test: /\.js$/,
+	loader: 'babel-loader',
+	exclude: '/node_modules/',
+      },
+    ]
+  }
+};
