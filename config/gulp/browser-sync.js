7c06b5a3cd611d0e085b49453f93cfd7ecd7cbcb
Samuel Tschümperlin <samuel.tschuemperlin@zeix.com>
2017-10-06 10:24:36 +0200

    PNG-9 connect browser sync to webpack config

--- config/gulp/browser-sync.js
+++ config/gulp/browser-sync.js
@@ -1,10 +1,22 @@
-var browserSync = require('browser-sync');
 var gulp = require('gulp');
+var webpack = require('webpack');
+var webpackDevMiddleware = require('webpack-dev-middleware');
+var webpackHotMiddleware = require('webpack-hot-middleware');
+var browserSync = require('browser-sync');
+var webpackConfig = require('../webpack/webpack.dev.conf');
+var bundler = webpack(webpackConfig);
 
 gulp.task('browser-sync', function() {
   browserSync.init({
     server: {
-      baseDir: "./.tmp"
+      baseDir: "./.tmp",
+      middleware: [
+	webpackDevMiddleware(bundler, {
+	    publicPath: webpackConfig.output.publicPath,
+	    stats: { colors: true }
+	}),
+	webpackHotMiddleware(bundler)
+      ]
     }
   })
 });
