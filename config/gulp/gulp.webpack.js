b36fae15e7bc470375c9f7144ba8b8e2a90345cc
Samuel Tschümperlin <samuel.tschuemperlin@zeix.com>
2017-10-17 13:48:36 +0200

    PNG-25 load the right webpack config with process.env, add scripts to package json

--- config/gulp/gulp.webpack.js
+++ config/gulp/gulp.webpack.js
@@ -3,8 +3,10 @@ var gulp = require('gulp');
 gulp.task('webpack', function() {
   var webpack = require('webpack-stream');
   var path = require('path');
-  var webpackConfig = require('../webpack/webpack.dev.conf');
+  var webpackConfig = process.env.NODE_ENV === 'development' ? require('../webpack/webpack.dev.conf') : require('../webpack/webpack.prod.conf');
   var pathsHelper = require('../helpers/paths.helper');
+
+  console.log(webpackConfig);
 
   return gulp.src([pathsHelper.src])
     .pipe(webpack(webpackConfig))
