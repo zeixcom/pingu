87aa148807e2afb93682880842876d6cc2ecbd0c
Marcel Bührig <marcel.buehrig@zeix.com>
2017-11-02 13:15:56 +0100

    Added todo comment

--- config/gulp/gulp.watch.js
+++ config/gulp/gulp.watch.js
@@ -3,5 +3,6 @@ var gulp = require('gulp');
 gulp.task('watch', function() {
   var pathsHelper = require('../helpers/paths.helper');
 
+  // @TODO: Remove preview from watch for final version
   gulp.watch([`${pathsHelper.src}/**/*.twig`, `${pathsHelper.src}/**/*.yml`], ['twig', 'preview']);
 });
