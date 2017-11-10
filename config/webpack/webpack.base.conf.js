c744c721dcc6bb058133a8d2e51d1964c2f7d9fa
Samuel Tschümperlin <samuel.tschuemperlin@zeix.com>
2017-11-10 14:28:51 +0100

    PNG-34 extend webpack with support for fonts, copy other assets

--- config/webpack/webpack.base.conf.js
+++ config/webpack/webpack.base.conf.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var pathsHelper = require('../helpers/paths.helper');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -29,6 +30,28 @@ module.exports = {
         loader: 'babel-loader',
         exclude: '/node_modules/',
       },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          context: pathsHelper.src,
+          name : '[path][hash].[ext]'
+        }
+      },
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: pathsHelper.assets,
+        to: pathsHelper.assetsPath('assets'),
+        ignore: ['css/*', 'js/*', 'fonts/*']
+      },
+      {
+        from: pathsHelper.previewAssets,
+        to: pathsHelper.assetsPath('preview/assets'),
+        ignore: ['css/*', 'js/*', 'fonts/*']
+      }
+    ])
+  ]
 };
